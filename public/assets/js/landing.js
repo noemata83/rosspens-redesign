30bc21c09ebe8991d16b28456ecd07fa49731572
Tucker McKinney <noemata83@gmail.com>
2017-12-08 17:53:23 +0000

    Fix landing styles

--- public/assets/js/landing.js
+++ public/assets/js/landing.js
@@ -20,7 +20,7 @@ function debounce(func, wait = 5, immediate = true) {
 }
 
 function checkSlide(e) {
-    if (window.scrollY <= 500) {
+    if (window.scrollY <= 500 && document.clientWidth >= 1050) {
         nav.style.backgroundColor = `rgba(58,71,73,${0.8 + (0.2 * window.scrollY/500)} )`;
         dropdowns.forEach(dropdown => dropdown.style.backgroundColor = `rgba(58,71,73,${0.8 + (0.2 * window.scrollY/500)})`);
     } else {
