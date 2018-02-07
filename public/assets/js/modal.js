9d1040ddbea8350ccf504ffd4877dd4e2f84bd22
Tucker McKinney <noemata83@gmail.com>
2018-02-07 16:58:59 +0000

    Implement modal gallery on show pages

---
+++ public/assets/js/modal.js
@@ -0,0 +1,35 @@
+function openModal() {
+    document.getElementById('penModal').style.display="inline-block";
+}
+
+function closeModal() {
+    document.getElementById('penModal').style.display="none";
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName('modal__slide');
+    console.dir(slides);
+    const dots = document.getElementsByClassName('modal__demo');
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1 ) { slideIndex = slides.length; }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++ ) {
+        dots[i].className = dots[i].className.replace(" modal__demo--active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " modal__demo--active";
+}
