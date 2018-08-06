89131cdbc0d940551637b38b8890e1178ebc7d6e
Tucker McKinney <noemata83@gmail.com>
2018-08-06 00:56:28 -0400

    Updated .gitignore for local dev

--- routes/message.js
+++ routes/message.js
@@ -13,7 +13,6 @@ router.get('/', isLoggedIn, (req, res) => {
 router.put('/', isLoggedIn, (req,res) => {
     req.body.message.content = req.sanitize(req.body.message.content);
     const id = req.body.message.id;
-    console.log(id);
     Message.findByIdAndUpdate(id, req.body.message, (err) => {
         if (err) {
             res.send("Something went wrong. Please try your request again. If the problem persists, contact the system administrator.");
