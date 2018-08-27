e955636b9e9717aacccdd526cb73bff571a3c2d7
Tucker McKinney <noemata83@gmail.com>
2018-08-27 11:36:40 -0400

    Diagnosing additional Heroku Redis problems

--- services/cache.js
+++ services/cache.js
@@ -20,17 +20,18 @@ mongoose.Query.prototype.exec = async function() {
 
   const key = JSON.stringify(Object.assign({}, this.getQuery(), {
     collection: this.mongooseCollection.name,
-  })).replace(/"/g, "'");
+  }));
 
   // check redis to see if we have a value for the key created
 
   const cachedValue = await client.hget(this.hashKey, key);
 
   if (cachedValue) {
-    const doc = JSON.parse(cachedValue.replace(/'/g, '"'));
+    console.log(cachedValue);
+    const doc = JSON.parse(cachedValue);
     return Array.isArray(doc) 
       ? doc.map(record => new this.model(record))
-      : new this.model(JSON.parse(cachedValue.replace(/'/g, '"')));
+      : new this.model(JSON.parse(cachedValue));
   }
 
   // if not, execute the query as normal, return and save the result in redis.
@@ -38,8 +39,8 @@ mongoose.Query.prototype.exec = async function() {
   const result = await exec.apply(this, arguments);
   console.log("hashKey: ", this.hashKey);
   console.log("key", key);
-  console.log("value", JSON.stringify(result).replace(/"/g, "'"));
-  client.hset(this.hashKey, key, JSON.stringify(result).replace(/"/g, "'"));
+  console.log("value", JSON.stringify(result));
+  client.hset(this.hashKey, key, JSON.stringify(result));
   return result;
 }
 
